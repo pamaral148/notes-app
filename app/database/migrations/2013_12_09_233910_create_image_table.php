aae5140c17e2552288ed85c574c7931664f9cf87
pamaral148 <pamaral148@gmail.com>
2013-12-09 15:42:23 -0800

    img table migration

---
+++ app/database/migrations/2013_12_09_233910_create_image_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('images', function(Blueprint $table)
+		{
+			$table->increments('id');
+                        $table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+                        $table->text('caption', 45);
+                        $table->binary('image');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('images');
+	}
+
+}
